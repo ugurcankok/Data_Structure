f0a216a887d09e3ca796949b661fc2c1f639aeb2
ugurcankok <ugurcankok0@gmail.com>
2023-08-24 10:31:19 +0300

    Add remove and insert function depends on index

--- src/Linked List/linkedList.py
+++ src/Linked List/linkedList.py
@@ -54,6 +54,45 @@ class LinkedList:
 
         return count
 
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+
 if __name__ == "__main__":
     ll = LinkedList()
     ll.insert_at_begining(5)
