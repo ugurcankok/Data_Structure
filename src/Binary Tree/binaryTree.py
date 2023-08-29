d2464d5ed58d5dcaccfa3ca72114c028dfdf85f4
ugurcankok <ugurcankok0@gmail.com>
2023-08-29 10:13:12 +0300

    Add delete node function

--- src/Binary Tree/binaryTree.py
+++ src/Binary Tree/binaryTree.py
@@ -82,6 +82,28 @@ class BinarySearchTreeNode:
             return self.data
 
         return self.left.find_min()
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+
+        return self
+
 def build_tree(elements):
     print("Building tree with these elements:",elements)
     root = BinarySearchTreeNode(elements[0])
